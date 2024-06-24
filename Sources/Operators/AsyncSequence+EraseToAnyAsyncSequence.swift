704f025fd06d1003adef374f8e031714871c5689
Kirsten Konya (HAU-ED-EXT) <Konya.Kirsten@liebherr.com>
2024-06-24 11:36:37 +0200

    Swift 6 Support

--- Sources/Operators/AsyncSequence+EraseToAnyAsyncSequence.swift
+++ Sources/Operators/AsyncSequence+EraseToAnyAsyncSequence.swift
@@ -1,44 +1,50 @@
 //
 //  AsyncSequence+EraseToAnyAsyncSequence.swift
-//  
+//
 //
 //  Created by Thibault Wittemberg on 31/12/2021.
 //
 
-public extension AsyncSequence {
-  /// Type erase the AsyncSequence into an AnyAsyncSequence.
-  /// - Returns: A type erased AsyncSequence.
-  func eraseToAnyAsyncSequence() -> AnyAsyncSequence<Element> where Self: Sendable {
-    AnyAsyncSequence(self)
-  }
+public extension AsyncSequence where Element: Sendable {
+    /// Type erase the AsyncSequence into an AnyAsyncSequence.
+    /// - Returns: A type erased AsyncSequence.
+    func eraseToAnyAsyncSequence() -> AnyAsyncSequence<Element> where Self: Sendable {
+        AnyAsyncSequence(self)
+    }
 }
 
 /// Type erased version of an AsyncSequence.
 public struct AnyAsyncSequence<Element>: AsyncSequence {
-  public typealias AsyncIterator = AnyAsyncIterator<Element>
+    public typealias AsyncIterator = AnyAsyncIterator<Element>
 
-  private let makeAsyncIteratorClosure: @Sendable () -> AsyncIterator
+    private let makeAsyncIteratorClosure: @Sendable () -> AsyncIterator
 
-  public init<Base: AsyncSequence>(_ base: Base) where Base.Element == Element, Base: Sendable {
-    self.makeAsyncIteratorClosure = { AnyAsyncIterator(base: base.makeAsyncIterator()) }
-  }
+    public init<Base: AsyncSequence>(_ base: Base) where Base.Element == Element, Base: Sendable {
+        self.makeAsyncIteratorClosure = { AnyAsyncIterator(base: base.makeAsyncIterator()) }
+    }
 
-  public func makeAsyncIterator() -> AsyncIterator {
-    self.makeAsyncIteratorClosure()
-  }
+    public func makeAsyncIterator() -> AsyncIterator {
+        self.makeAsyncIteratorClosure()
+    }
+}
+
+extension AnyAsyncSequence: @unchecked Sendable where Element: Sendable {
 }
 
 public struct AnyAsyncIterator<Element>: AsyncIteratorProtocol {
-  public typealias Element = Element
+    public typealias Element = Element
 
-  private let nextClosure: () async throws -> Element?
+    private let nextClosure: () async throws -> Element?
 
-  public init<Base: AsyncIteratorProtocol>(base: Base) where Base.Element == Element {
-    var mutableBase = base
-    self.nextClosure = { try await mutableBase.next() }
-  }
+    public init<Base: AsyncIteratorProtocol>(base: Base) where Base.Element == Element {
+        var mutableBase = base
+        self.nextClosure = { try await mutableBase.next() }
+    }
 
-  public mutating func next() async throws -> Element? {
-    try await self.nextClosure()
-  }
+    public mutating func next() async throws -> Element? {
+        try await self.nextClosure()
+    }
+}
+
+extension AnyAsyncIterator: @unchecked Sendable where Element: Sendable {
 }
